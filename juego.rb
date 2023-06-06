cec02c4ae664c1de4f07020b187271b057fb151d
bpereira74 <67373077+bpereira74@users.noreply.github.com>
2023-06-06 16:58:26 +0000

    Subida archivo juego.rb

---
+++ juego.rb
@@ -0,0 +1,23 @@
+puts 'Hola, este es el juego piedra, papel o tijera'
+puts 'Introduzca una de las tres opciones: piedra, papel o tijeras'
+opcion_usuario=gets.chomp.downcase
+opcion=['piedra','papel','tijeras']
+opcion_maquina=opcion.sample
+puts (" Yo jugué: #{opcion_maquina}")
+  if opcion_usuario==opcion_maquina
+  puts("Elegiste #{opcion_usuario} y yo elegí #{opcion_maquina}:  Entonces empatamos")
+  else
+  if opcion_usuario=="piedra" && opcion_maquina=="tijeras" ||
+opcion_usuario=="papel" && opcion_maquina=="piedra" ||
+opcion_usuario=="tijeras" && opcion_maquina=="papel"
+    puts("Elegiste #{opcion_usuario} y yo elegí #{opcion_maquina}:Ganaste")
+ else
+    if opcion_usuario=="tijeras" && opcion_usuarioquina=="piedra" ||
+opcion_usuario=="piedra" && opcion_maquina=="papel" ||
+opcion_usuario=="papel" && opcion_maquina=="tijera"
+    print("Elegiste #{opcion_usuario} y yo elegí #{opcion_maquina}:Perdiste")
+    else
+  puts("Debes ingresar una opcion válida")    
+  end
+  end
+  end
